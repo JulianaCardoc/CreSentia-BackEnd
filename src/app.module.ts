13518756298d08663add0abf0737b679252a9579
Juliana Cardona <julicardonco@hotmail.com>
2024-08-26 22:04:49 -0500

    Conflict solving with membership module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,37 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SalesModule } from './sales/sales.module';
-import { MembershipsModule } from './memberships/memberships.module';
-import { UserMembershipController } from './user-membership/controllers/userMembership.controller';
-import { UserMembershipService } from './user-membership/services/userMembership.service';
-import { UserMembershipModule } from './user-membership/module/module.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { environments } from './environments';
 import config from './config';
+import { MembershipsModule } from './memberships/memberships.module';
+import { UserMembershipModule } from './user-membership/module/module.module';
+import { UserMembershipController } from './user-membership/controllers/userMembership.controller';
+import { UserMembershipService } from './user-membership/services/userMembership.service';
 
 @Module({
-  imports: [UsersModule, SalesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    SalesModule,
+    DatabaseModule,
+    MembershipsModule,
+    UserMembershipModule,
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [AppController, UserMembershipController],
+  providers: [AppService, UserMembershipService],
 })
 export class AppModule {}
